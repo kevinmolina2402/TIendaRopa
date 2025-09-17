0362e81b406b398d6d5a67f292acf45e6836f13f
Sara Pabón González <sarapabon1123527@correo.itm.edu.co>
2025-09-16 22:43:12 -0500

    Commit 2: Cambios realizados por Sara Pabón - Cambio de la ruta_json en DatosGenerales.cs y 4.Pruebas unitarias

--- lib_dominio/Nucleo/DatosGenerales.cs
+++ lib_dominio/Nucleo/DatosGenerales.cs
@@ -2,7 +2,7 @@
 {
     public class DatosGenerales
     {
-        public static string ruta_json = @"C:\Users\AA\Desktop\Comidas\config.txt";
+        public static string ruta_json = @"C:\Users\AA\Desktop\TIendaRopa\config.txt";
         public static bool usa_azure = false;
         public static string clave = "EVBgi345936456ghhVBJGtgni-fytsidi3456678jhgUTytutyiiyi";
         public static string usuario_datos = EncriptarConversor.Encriptar("Test.Trghhjsgdj");
